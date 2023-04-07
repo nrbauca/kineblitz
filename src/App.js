7845a2f2eb547e0a0bb276ea1b5a4051d7a74cdb
nrbauca <98324456+nrbauca@users.noreply.github.com>
2023-04-07 17:44:22 +0800

    Routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import NavigationBar from "./Components/NavigationBar";
+import Home from './Pages/Home';
+import Kineblitz from './Pages/Kineblitz';
+import AboutUs from './Pages/AboutUs';
 import LogIn from "./Pages/LogIn";
 import './App.css';
 
@@ -10,8 +13,10 @@ function App() {
       <BrowserRouter>
         <NavigationBar />
         <Routes>
-          <Route path="/" element={<Navigate to="/ome" />} />
-          
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/kineblitz" element={<Kineblitz />} />
+          <Route path="/aboutus" element={<AboutUs />} />
           <Route path="/login" element={<LogIn />} />
         </Routes>
       </BrowserRouter>
