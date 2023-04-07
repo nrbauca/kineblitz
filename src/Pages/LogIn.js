87a8aa6c602effdfa68dbb3a107c76345fce21d7
nrbauca <98324456+nrbauca@users.noreply.github.com>
2023-04-07 12:19:11 +0800

    Update

--- src/Pages/LogIn.js
+++ src/Pages/LogIn.js
@@ -1,15 +1,25 @@
 import React from 'react'
+import submit from '../App'
 
 export default function LogIn() {
   return (
-    <div>
-        <div className="flex item-center pt-60">
-            <form action="">
+    <div className="py-36">  
+        <div className="flex justify-center w-[432px] h-[507px] bg-[#1d3578]">
+            <form onSubmit={submit}>
                     <p>SIGN IN</p>
-                    <label for="email adress">
-                        Email Address
-                        <imput type="email" name="email-adress" id="email-adress" />
+                    <label for="email adress"> Email Address
+                        <input type="email" name="email-adress" id="email-adress" />
                     </label>
+
+                    <label for="password">Password
+                    <input type="password" name="password" id="password" />
+
+                    </label>
+
+                <p>Forgot Password?</p>
+                <p>Lorem ipsum dolor sit amet</p>
+
+                <button type="submit">SIGN IN</button>
             </form>
         </div>
     </div>
