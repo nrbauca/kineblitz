7471b9889f2d0a1cd72dad6dc13358aa3a8c830a
nrbauca <98324456+nrbauca@users.noreply.github.com>
2023-04-11 14:46:42 +0800

    NavigationBar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Home from './Pages/Home';
 import Kineblitz from './Pages/Kineblitz';
 import AboutUs from './Pages/AboutUs';
 import LogIn from "./Pages/LogIn";
+import Dashboard from './Pages/Dashboard'
+import UserDashboard from './Pages/UserDashboard'
+import Record from './Pages/Record'
 import './App.css';
 
 function App() {
@@ -18,6 +21,9 @@ function App() {
           <Route path="/kineblitz" element={<Kineblitz />} />
           <Route path="/aboutus" element={<AboutUs />} />
           <Route path="/login" element={<LogIn />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/userdashboard" element={<UserDashboard />} />
+          <Route path="/record" element={<Record />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Rectangle1 from '../Images/Rectangle 1.png'
 
 export default function NavigationBar() {
   return (
     <div>
-      <header>
+      <header className="font-['Outfit']">
         <nav className="absolute z-50 top-0 w-full flex justify-between pl-20 py-6 bg-[#1d3578] text-white">
-          <Link className="text-2xl font-semibold" to="/home">PENTABYTE</Link>
+          <div className="flex items-center">
+            <img src={Rectangle1} alt="pentabyte logo" className="w-[43px] h-[38px]" />
+            <Link className="text-2xl font-semibold" to="/home">PENTABYTE</Link>
+          </div>
           <div className="flex gap-16 items-center pr-20">
             <Link to="/kineblitz"><button className="text-base">KINEBLITZ</button></Link>
             <Link to="/aboutus"><button className="text-base">ABOUT US</button></Link>
             <Link to="/login"><button className="text-base">LOG IN</button></Link>
-            <Link to="/login"><button className="text-base">Dashboard (Temp)</button></Link>
-            <Link to="/login"><button className="text-base">LOG IN</button></Link>
-            <Link to="/login"><button className="text-base">LOG IN</button></Link>
+            <Link to="/dashboard"><button className="text-base">Dashboard (Temp)</button></Link>
+            <Link to="/userdashboard"><button className="text-base">UserDashboard (Temp)</button></Link>
+            <Link to="/record"><button className="text-base">Record (Temp)</button></Link>
           </div>
         </nav>
       </header>
