475530774710f5b5d80e281afb86fa573f75e50c
nrbauca <98324456+nrbauca@users.noreply.github.com>
2023-04-18 03:21:13 +0800

    NavigationBar/Main color

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -6,18 +6,18 @@ export default function NavigationBar() {
   return (
     <div>
       <header className="font-['Outfit']">
-        <nav className="absolute z-50 top-0 flex h-[80px] w-full justify-between text-white pl-14 bg-[#051F6A]">
+        <nav className="absolute z-50 top-0 w-full justify-between text-white pl-14 bg-[#051F6A] lg:flex lg:h-[80px]">
           <div className="flex items-center">
             <img src={pentabytelogo} alt="pentabyte logo" className="w-[65px] h-[60px]" />
             <Link className="text-3xl font-semibold" to="/home">PENTABYTE</Link>
           </div>
-          <div className="flex gap-6 items-center pr-14">
-            <Link to="/kineblitz"><button className="text-base">KINEBLITZ</button></Link>
-            <Link to="/aboutus"><button className="text-base">ABOUT US</button></Link>
-            <Link to="/login"><button className="text-base">LOG IN</button></Link>
-            <Link to="/dashboard"><button className="text-base">Dashboard (Temp)</button></Link>
-            <Link to="/userdashboard"><button className="text-base">UserDashboard (Temp)</button></Link>
-            <Link to="/record"><button className="text-base">Record (Temp)</button></Link>
+          <div className="items-center text-xs sm:pr-14 sm:text-base sm:flex md:gap-6">
+            <Link to="/kineblitz"><button className="pr-6 py-1">KINEBLITZ</button></Link>
+            <Link to="/aboutus"><button className="pr-6 py-1">ABOUT US</button></Link>
+            <Link to="/login"><button className="pr-6 py-1">LOG IN</button></Link>
+            <Link to="/dashboard"><button className="pr-6 py-1">Dashboard (Temp)</button></Link>
+            <Link to="/userdashboard"><button className="pr-6 py-1">UserDashboard (Temp)</button></Link>
+            <Link to="/record"><button>Record (Temp)</button></Link>
           </div>
         </nav>
       </header>
