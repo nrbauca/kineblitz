8a3c5510d53c072dd51b475668437c88fdee82f5
Nozomi Omura <nozomiomura23@gmail.com>
2023-04-30 01:35:20 +0800

    About Us Contents/Style

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -45,28 +45,40 @@ export default function AboutUs() {
             <div className="member-container row">
               <div className="member gen-button">
                 <img src alt="" />
-                <p>Name</p>
-                <p>Position</p>
+                <p className="member-name">NATHALIE BAUCA</p>
+                <p className="member-pos">PROJECT MANAGER</p>
+                <p className="member-des">She is responsible for planning, 
+                  organizing, and directing the project. 
+                  She handles the repository.</p>
               </div>
               <div className="member gen-button">
                 <img src alt="" />
-                <p>Name</p>
-                <p>Position</p>
+                <p className="member-name">MARK FRANCISCO</p>
+                <p className="member-pos">PROJECT ANALYST</p>
+                <p className="member-des">He oversees defining and developing 
+                  the contents needed for the project and raise solutions.</p>
               </div>
               <div className="member gen-button">
                 <img src alt="" />
-                <p>Name</p>
-                <p>Position</p>
+                <p className="member-name">JELLA NACARIO</p>
+                <p className="member-pos">PROJECT COORDINATOR</p>
+                <p className="member-des">She manages the deliverables according to 
+                  the decided plan and establishes the project schedule.</p>
               </div>
               <div className="member gen-button">
                 <img src alt="" />
-                <p>Name</p>
-                <p>Position</p>
+                <p className="member-name">NOZOMI OMURA</p>
+                <p className="member-pos">PROJECT SPONSOR</p>
+                <p className="member-des">She ensures the availability of resources for 
+                  the team and helps in communicating the project’s goals.</p>
               </div>
               <div className="member gen-button">
                 <img src alt="" />
-                <p>Name</p>
-                <p>Position</p>
+                <p className="member-name">RALPH<br />RAMOS</p>
+                <p className="member-pos">PROJECT SPONSOR</p>
+                <p className="member-des">He works closely with the Project Manager in
+                  making key business decisions and in the approval of the 
+                  project budget.</p>
               </div>
             </div>
           </div>
