c1c4746353ad08a0c90e03faef7faf2021e477c2
Nozomi Omura <nozomiomura23@gmail.com>
2023-05-02 04:40:39 +0800

    Kineblitz Page/Style

--- src/Pages/Kineblitz.js
+++ src/Pages/Kineblitz.js
@@ -2,8 +2,54 @@ import React from 'react';
 
 export default function Kineblitz() {
   return (
-    <div>
-        {/* Paste Kineblitz Page HTML Here */}
-    </div>
+    <main>
+        <div className="kineblitz container">
+          <h1>KINEBLITZ</h1>
+          <div className="kine row">
+            <div className="kineblitz-details">
+              <p className="kineblitz-title right">LOREM IPSUM</p>
+              <p className="kineblitz-description right">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                otam, facilis hic alias consequatur amet et optio sed 
+                consequuntur quis nihil? Repudiandae amet quisquam eos error 
+                magni rem sapiente perspiciatis maxime.
+              </p>
+            </div>
+            <img src alt="device pics???" />
+          </div>
+        </div>
+        <div className="how-it-works column">
+          <div className="how-it-works-container column">
+            <h1>HOW IT WORKS???</h1>
+            <div className="process-container row">
+              <div className="process gen-button">
+                <img src alt="" />
+                <p className="process-name">PROCESS #</p>
+                <p className="process-des">Process information</p>
+              </div>
+              <div className="process gen-button">
+                <img src alt="" />
+                <p className="process-name">PROCESS #</p>
+                <p className="process-des">Process information</p>
+              </div>
+              <div className="process gen-button">
+                <img src alt="" />
+                <p className="process-name">PROCESS #</p>
+                <p className="process-des">Process information</p>
+              </div>
+              <div className="process gen-button">
+                <img src alt="" />
+                <p className="process-name">PROCESS #</p>
+                <p className="process-des">Process information</p>
+              </div>
+              <div className="process gen-button">
+                <img src alt="" />
+                <p className="process-name">PROCESS #</p>
+                <p className="process-des">Process information</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
   )
 }
