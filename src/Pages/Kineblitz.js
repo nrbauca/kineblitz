bfd415bd5e79fea498117d752a5411f4bca0dab0
Nozomi Omura <nozomiomura23@gmail.com>
2023-05-05 13:24:36 +0800

    kineblitz content/css

--- src/Pages/Kineblitz.js
+++ src/Pages/Kineblitz.js
@@ -7,12 +7,14 @@ export default function Kineblitz() {
           <h1>KINEBLITZ</h1>
           <div className="kine row">
             <div className="kineblitz-details">
-              <p className="kineblitz-title right">LOREM IPSUM</p>
               <p className="kineblitz-description right">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                otam, facilis hic alias consequatur amet et optio sed 
-                consequuntur quis nihil? Repudiandae amet quisquam eos error 
-                magni rem sapiente perspiciatis maxime.
+              “Kineblitz,” a biofeedback game-based habilitation system designed specifically 
+              for children that are diagnosed with spastic cerebral palsy for the upper body. 
+              The game will be structured in such a way that it employs electromyographic feedback. 
+              By initially devising a microcontroller-based device capable of serving as a surface 
+              electromyography (EMG) sensor, muscle activity from the surface of the upper area of 
+              the body that is above the muscle on the skin shall be recorded and will serve as an 
+              input to be utilized to control the game.
               </p>
             </div>
             <img src alt="device pics???" />
@@ -24,17 +26,17 @@ export default function Kineblitz() {
           <h1>PATIENT PROCESS</h1>
           <div className="process-container row">
             <div className="patient-process gen-button">
-              <img src alt="" />
+              <img src alt="register" />
               <p className="process-name">REGISTER TO WEBAPP</p>
               <p className="process-des">Process information</p>
             </div>
             <div className="patient-process gen-button">
-              <img src alt="" />
+              <img src alt="testing" />
               <p className="process-name">TESTING PHASE</p>
               <p className="process-des">Process information</p>
             </div>
             <div className="patient-process gen-button">
-              <img src alt="" />
+              <img src alt="access" />
               <p className="process-name">ACCESS ACCOUNT</p>
               <p className="process-des">Process information</p>
             </div>
@@ -45,17 +47,17 @@ export default function Kineblitz() {
           <h1>PROFESSIONAL PROCESS</h1>
           <div className="process-container row">
             <div className="professional-process gen-button">
-              <img src alt="" />
+              <img src alt="professional" />
               <p className="process-name">PROFESSIONAL ACCOUNT</p>
               <p className="process-des">Process information</p>
             </div>
             <div className="professional-process gen-button">
-              <img src alt="" />
+              <img src alt="select-patient" />
               <p className="process-name">SELECT PATIENT RECORD</p>
               <p className="process-des">Process information</p>
             </div>
             <div className="professional-process gen-button">
-              <img src alt="" />
+              <img src alt="evaluate" />
               <p className="process-name">EVALUATE RECORD</p>
               <p className="process-des">Process information</p>
             </div>
